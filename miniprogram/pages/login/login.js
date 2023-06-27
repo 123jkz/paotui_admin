f0975b813dd49a0cb44683c4e636268be38344bc
123jkz <1436003760@qq.com>
2023-06-27 14:27:05 +0800

    登录

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,16 +1,59 @@
 // pages/login/login.js
+const db = wx.cloud.database()
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        name:'',
+        mypassword:''
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
+    getName(e){
+        let name=this.data.name;
+        name= e.detail.value;
+        this.setData({
+            name,
+        })
+    },
+
+    getpassword(e){
+        let password=this.data.password;
+        password= e.detail.value;
+        this.setData({
+            password,
+        })
+    },
+
+    check(){
+        var that=this.data;
+        let name=that.name;
+        let password=that.password;
+        console.log(name,password);
+        db.collection('admininfo').where({
+            name:name,
+            password:password,
+        }).get({
+            success(res){
+                if(res.data.length==0){
+                    wx.showToast({
+                      title: '登录信息错误',
+                      icon:'error'
+                    })
+                    return;
+                }
+                wx.redirectTo({
+                  url: '../admin/admin',
+                })
+            }
+        })
+    },
+
+
     onLoad(options) {
 
     },
